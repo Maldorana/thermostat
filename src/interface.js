77d24ee5c0c75562e23a0138c51805ca030afa6f
Jessica Bulman <jessicabulman97@gmail.com>
2021-09-28 17:36:45 +0100

    interface.js

---
+++ src/interface.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const updateTemperature() = () => {
+    document.querySelector('#temperature').innerText = thermostat.temperature;
+  }
+  const thermostat = new Thermostat();
+  updateTemperature();
+
+document.querySelector('#temperature-up').addEventListener('click', () => {
+  thermostat.up();
+  updateTemperature();
+});
+
+document.querySelector('#temperature-down').addEventListener('click', () => {
+  thermostat.down();
+  updateTemperature();
+});
