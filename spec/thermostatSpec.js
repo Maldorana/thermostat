6b9a0b16d0124712bc9fa49ed06923f44b27182a
Sophie Bonneval <maldorana@gmail.com>
2021-09-28 16:31:34 +0100

    Power Saving Mode, maximum and minimum temperatures

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -21,4 +21,45 @@ describe('Thermostat', () => {
     thermostat.down();
     expect(thermostat.getCurrentTemperature()).toEqual(19)
   });
+
+  it('has a minimum of 10 degrees', () => {
+    for (let i = 0; i < 11; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10)
+  });
+
+  it('has power saving mode on by default', () => {
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  });
+
+  it('turns off the power saving mode', () => {
+    thermostat.switchPowerSavingModeOff();
+    expect(thermostat.isPowerSavingModeOn()).toBe(false);
+  });
+
+  it('turns back on the power saving mode', () => {
+    thermostat.switchPowerSavingModeOff();
+    thermostat.switchPowerSavingModeOn();
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  });
+
+  describe('when power saving mode is on', () => {
+    it('has a maximum of 25 degrees', () => {
+      for (let i = 0; i < 6; i++) {
+          thermostat.up();
+        }
+        expect(thermostat.getCurrentTemperature()).toEqual(25);
+    });
+  });
+
+  describe('when power saving mode is off', () => {
+    it('has a maximum temperature of 32 degrees', () => {
+      thermostat.switchPowerSavingModeOff();
+      for (let i = 0; i < 13; i++) {
+        thermostat.up();
+      }
+      expect(thermostat.getCurrentTemperature()).toEqual(32);
+    });
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,14 +1,45 @@
+'use strict';
+
 class Thermostat {
   constructor() {
+    this.MINIMUM_TEMPERATURE = 10;
+    this.MAXIMUM_TEMPERATURE_PSM_ON = 25;
+    this.MAXIMUM_TEMPERATURE_PSM_OFF = 32;
     this.temperature = 20;
+    this.PowerSavingMode = true;
   }
   getCurrentTemperature() {
-    return this.temperature
+    return this.temperature;
+  }
+  isMinimumTemperature() {
+    return this.temperature === this.MINIMUM_TEMPERATURE;
+  }
+  isMaximumTemperature() {
+    if (this.isPowerSavingModeOn()) {
+      return this.temperature === this.MAXIMUM_TEMPERATURE_PSM_ON;
+    } else {
+      return this.temperature === this.MAXIMUM_TEMPERATURE_PSM_OFF;
+    }
+  }
+  isPowerSavingModeOn() {
+    return this.PowerSavingMode === true;
+  }
+  switchPowerSavingModeOff() {
+    this.PowerSavingMode = false;
+  }
+  switchPowerSavingModeOn() {
+    this.PowerSavingMode = true;
   }
   up() {
+    if (this.isMaximumTemperature()) {
+      return;
+    }
     this.temperature += 1;
   }
   down() {
+    if (this.isMinimumTemperature()) {
+      return;
+    }
     this.temperature -= 1;
   }
 };
